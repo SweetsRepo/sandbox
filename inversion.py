3e90764f857dca453f54f21f1dae41624f82b849
Christopher Sweet <crs4263@rit.edu>
2017-09-18 13:32:35 -0400

    Update inversion based off pull request from standalone repo

--- inversion.py
+++ inversion.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 """inversion.py is a Pillow Script designed to find all the images in a
    given directory and invert them for usage in formal technical reports """
 
 __author__ = "Christopher Sweet"
+__author__ = "Thomas Cenova"
 
 from PIL import Image
 from PIL import ImageOps
@@ -14,11 +16,18 @@ import argparse
 def invert(i_dir:str, o_dir:str):
     for f in os.listdir(i_dir):
         try:
-            image = Image.open(i_dir + f)
-            inverted_image = ImageOps.invert(image)
-            inverted_image.save(o_dir + f)
-        except TypeError as e:
-            raise e
+            image = Image.open(i_dir +'/'+ f)
+            if image.mode == 'RGBA':
+                r,g,b,a = image.split()
+                rgb_image = Image.merge('RGB', (r,g,b))
+                inverted_image = ImageOps.invert(rgb_image)
+                r2,g2,b2 = inverted_image.split()
+                inverted_image = Image.merge('RGBA', (r2,g2,b2,a))
+            else:
+                inverted_image = ImageOps.invert(image)
+            inverted_image.save(o_dir +'/'+ f)
+        except IsADirectoryError as e:
+            pass
 
 parser = argparse.ArgumentParser(description="Take in directory locations")
 parser.add_argument('-I', '--i_dir', type=str, nargs='?', default='./images/',
@@ -27,22 +36,18 @@ parser.add_argument('-O', '--o_dir', type=str, nargs='?',
     action='store', help="Output directory to place all inverted images in")
 
 args = parser.parse_args()
-#Check input for directory structure and append / if needed
-if args.i_dir[-1] != '/':
-    args.i_dir = args.i_dir+'/'
-if args.o_dir[-1] != '/':
-    args.o_dir = args.o_dir+'/'
 
+#Ensure Output Directory Exists before calling invert
 try:
     if args.i_dir and args.o_dir:
+        if not os.path.isdir(args.o_dir):
+            os.mkdir(args.o_dir)
         invert(args.i_dir, args.o_dir)
     elif args.i_dir and not args.o_dir:
-        o_dir = '../'+i_dir+'/inverted'
-        if os.isdir(o_dir):
-            invert(args.i_dir, o_dir)
-        else:
+        o_dir = args.i_dir+'/inverted'
+        if not os.path.isdir(o_dir):
             os.mkdir(o_dir)
-            invert(args.i_dir, o_dir)
+        invert(args.i_dir, o_dir)
     else:
         raise FileNotFoundError("Invalid Path Given")
 except FileNotFoundError as e:
